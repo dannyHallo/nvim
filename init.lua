28b9367d93c02b06b62e1fb2779fe3f98d91dd64
DannyHallo <ruitian0716@gmail.com>
2023-12-20 14:50:08 +0800

    plugin surround

--- init.lua
+++ init.lua
@@ -1,44 +1,14 @@
 -- for macos, place it in ~/.config/nvim/
 -- for windows, place it in %APPDATA%\nvim\
+vim.cmd("autocmd BufEnter * set formatoptions-=cro")
 
-vim.cmd("autocmd BufEnter * set formatoptions-=cro") 
-
-local lazypath= vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system(
+        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+         lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
-  {
-    "folke/flash.nvim",
-    enabled = true,
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
-    -- stylua: ignore
-    keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    },
-  },
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-  }
-)
-
+require("lazy").setup("plugins")
