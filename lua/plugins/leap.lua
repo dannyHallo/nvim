0494e50f0c7f87c1f79c2c61ab33b021381956f1
danny <ruitian0716@gmail.com>
2024-09-09 13:54:51 +0800

    use leap to replace problematic flash

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,8 +1,8 @@
 return {
     "ggandor/leap.nvim",
-    enabled = false, -- this extension has collision with flash-nvim (ds), so that it is disabled by defaults
+    enabled = true, -- this extension has collision with flash-nvim (ds), so that it is disabled by defaults
     event = "VeryLazy",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = "*", -- Use for stability; omit to use `main` branch for latest features
     keys = {"E", "R", "W", "dE", "dR", "yE", "yR", "cE", "cR"},
     config = function()
         require("leap").add_default_mappings()
